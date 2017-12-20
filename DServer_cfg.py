c975339d00bdfe15a47815274c88d14828a7e6bc
Shutofsky <shutofsky@gmail.com>
2017-12-20 16:57:03 +0300

    Add files via upload

--- DServer_cfg.py
+++ DServer_cfg.py
@@ -14,41 +14,56 @@ menuFrame = []
 menuLabel = []
 menuBody = []
 bodyFrame = []
+bodyLabel = []
 bodyHead = []
 bodyText = []
 bodyScroll = []
+termButton = []
+
+def terminalUpdateRequest(num,ip):
+    print (num)
+    print (ip)
+
+root = Tk()
+
+
 
 i = 0
 conn = sqlite3.connect(dbName)
 req = conn.cursor()
 for row in req.execute("SELECT * from term_status ORDER BY Id"):
-    termWindow.append(Tk())
-    termWindow[i].geometry('600x400')
+    termWindow.append(Toplevel())
+#    termWindow[i].geometry('600x400')
     headTitle = u'Терминал ' + row[0]
     termWindow[i].title(headTitle)
     menuFrame.append(Frame(termWindow[i]))
-    menuLabel.append(Label(menuFrame[i], text=u'Пункты меню'))
-    menuBody.append(Entry(menuFrame[i], text = row[5]))
+    menuLabel.append(Label(menuFrame[i], text=u'Пункты меню: '))
+    menuLabel[i].grid(row=0, column=0, sticky=W)
+    menuBody.append(Entry(menuFrame[i], width=50))
+    menuBody[i].insert(0, row[5])
+    menuBody[i].grid(row=0, column=1, sticky=E)
+    bodyLabel.append(Label(menuFrame[i], text=u'Заголовок: '))
+    bodyLabel[i].grid(row=1, column=0, sticky=W)
+    bodyHead.append(Entry(menuFrame[i], width=50))
+    bodyHead[i].insert(0, row[7])
+    bodyHead[i].grid(row=1, column=1, sticky=E)
     menuFrame[i].grid(row=0, column=0)
+
     bodyFrame.append(Frame(termWindow[i]))
-    bodyHead.append(Entry(bodyFrame[i], text=row[7]))
-    bodyText.append(Text(bodyFrame[i], width=62, height=5, wrap=WORD, font='arial 8'))
+    bodyText.append(Text(bodyFrame[i], width=62, height=10, wrap=WORD, font='arial 8'))
     bodyText[i].insert(END, row[8])
+    bodyText[i].grid(row=1,column=0, sticky=W)
     bodyScroll.append(Scrollbar(bodyFrame[i], command=bodyText[i].yview))
-    bodyFrame[i].grid(row=1, column=0)
-#    termLive[row[0]] = row[1]
-#    termHack[row[0]] = row[2]
-#    termLock[row[0]] = row[3]
-#    termOper[row[0]] = row[4]
-#    termMenu[row[0]] = row[5]
-#    termMsgHead[row[0]] = row[7]
-#    termMsgBody[row[0]] = row[8]
+    bodyScroll[i].grid(row=1, column=1, sticky=N+S)
+    bodyFrame[i].grid(row=1, column = 0)
+    termButton.append(Button(termWindow[i], text='Обновить всё!', width=50, \
+                                 command=lambda num=i,ip=row[0]:terminalUpdateRequest(num,ip)))
+    termButton[i].grid(row=2, column=0)
+
     i += 1
 
 numTerms = i
 conn.close()
 
-i = 0
-while i < numTerms:
-    termWindow[i].mainloop()
-    i += 1
+root.mainloop()
+
